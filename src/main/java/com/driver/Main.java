56efb7221857a1e4d5a64beee75704ac5628ca53
Ankur <ankurmandal.official@gmail.com>
2023-10-19 14:12:36 +0530

    Attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
 //        System.out.println(obj.name);
 //        :6:12 java: name has private access in com.driver.RWOnly
 //        :7:31 java: name has private access in com.driver.RWOnly
-        obj.setter("Ankur");
-        System.out.println(obj.getter());
+        obj.setName("Ankur");
+        System.out.println(obj.getName());
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,6 +3,6 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    public void setter (String name){this.name=name;}
-    public String getter(){return this.name;}
+    public void setName (String name){this.name=name;}
+    public String getName(){return this.name;}
 }
